db596b63af69ced271d26804e4e3143ae0e8cb3a
raphaeldeoliveira <rafaolive3003@gmail.com>
2024-01-28 22:24:27 -0300

    implementação do formulario (componente Register)

--- src/Register.jsx
+++ src/Register.jsx
@@ -2,8 +2,8 @@ import React from "react"
 import { useRef, useEffect, useState } from "react"
 
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]){8,24}$/;
-
+//const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]){8,24}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%])[a-zA-Z0-9!@#$%]{8,24}$/;
 
 // criar uma logica na label "username" para quando o regex bater ele mostrar um icone do react icon que demonstre que esta correto (check) e caso contrario um icone de errado (uncheck)
 
@@ -50,11 +50,26 @@ export default function Register() {
         setErrMsg('');
     }, [user, pwd, matchPwd])
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(user, pwd);
+        console.log(`user: ${user} password: ${pwd}`);
+        setSucess(true);
+        // faz o post no backend com API fetch (try catch)
+    }
+
     return (
-        <section>
+        <>
+            {sucess ? (
+                <section>
+                    <h1>Sucess!</h1>
+                    {/* Inves dessa section com um link para a pagina de login podemos usar o Redirect do react router para redirecionar para o componente de Login */}
+                </section>
+            ) : (
+                <section>
             <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
             <h1>Register</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="username">Username: </label>
                 <input 
                     type="text" 
@@ -90,7 +105,34 @@ export default function Register() {
                     Must include uppercase and lowercase letters, a number and a special character.<br />
                     Allowed special characters: ! @ # $ %
                 </p>
+
+                <label htmlFor="confirm_pwd">Password: </label>
+                <input 
+                    type="password" 
+                    id="confirm_pwd" 
+                    onChange={(e) => setMatchPwd(e.target.value)} 
+                    required 
+                    aria-invalid={validMatch ? "false" : "true"} 
+                    aria-describedby="confirmnote" 
+                    onFocus={() => setMatchFocus(true)} 
+                    onBlur={() => setMatchFocus(false)}
+                />
+                <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
+                    Must match the first password input field.
+                </p>
+
+                <button disabled={!validName || !validPwd || !validMatch ? true : false}>Sign Up</button>
             </form>
+            <p>
+                Already registered?<br />
+                <span className="line">
+                    {/* aqui coloca o Link da Route para a pagina de criar conta */}
+                    <a href="#">Sign in</a>
+                </span>
+            </p>
         </section>
+            )}
+        </>
+        
     )
 }
